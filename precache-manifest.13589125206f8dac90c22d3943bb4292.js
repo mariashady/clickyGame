a66617dc588cb5d85e3f77058522755ba7423933
Maria Shady <maria.shady@yahoo.com>
2019-11-08 14:18:01 -0500

    Updates

---
+++ precache-manifest.13589125206f8dac90c22d3943bb4292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a8b20da1d133708e38ac6ab2ae9e15",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "d1a39bfd3d50db51b083",
+    "url": "/clickygame/static/css/main.900811d5.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/clickygame/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "d1a39bfd3d50db51b083",
+    "url": "/clickygame/static/js/main.7fc78336.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  }
+]);
